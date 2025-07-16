f1d2070e2843e963a034486fa2cdd82a2fd6d9b8
LineXic <linexic@qq.com>
2025-07-16 19:04:12 +0800

    twikoo

---
+++ .vercel/output/static/_astro/links.astro_astro_type_script_index_0_lang.DPP6ZTOR.js
@@ -0,0 +1,1 @@
+import{t as o}from"./twikoo.all.min.BgM9d8go.js";o({envId:"https://talk.linexic.top/.netlify/functions/twikoo",el:"#tcomment",lang:"zh-cn"}).then(()=>{console.log("comment loading success")});
