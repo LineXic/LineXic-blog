4823e1e1d4966f4e231d93b9465785fa681abce1
LineXic <linexic@qq.com>
2025-08-18 15:11:47 +0800

    添加鱼塘

---
+++ public/link.js
@@ -0,0 +1,30 @@
+import YML from 'yamljs';
+import fs from 'fs';
+
+const blacklist = ["承挨的小屋", "bbb-lsy07", "友站名称3"]; // 由于某种原因，不想订阅的列表
+
+let friends = [],
+    data_f = YML.parse(fs.readFileSync('public/link.yml').toString().replace(/(?<=rss:)\s*\n/g, ' ""\n'));
+
+data_f.forEach((entry, index) => {
+    let lastIndex = 2;
+    if (index < lastIndex) {
+        const filteredLinkList = entry.link_list.filter(linkItem => !blacklist.includes(linkItem.name));
+        friends = friends.concat(filteredLinkList);
+    }
+});
+
+// 根据规定的格式构建 JSON 数据
+const friendData = {
+    friends: friends.map(item => {
+        return [item.name, item.link, item.avatar];
+    })
+};
+
+// 将 JSON 对象转换为字符串
+const friendJSON = JSON.stringify(friendData, null, 2);
+
+// 写入 friend.json 文件
+fs.writeFileSync('./public/friend.json', friendJSON);
+
+console.log('friend.json 文件已生成。');
--- src/consts.ts
+++ src/consts.ts
@@ -29,6 +29,7 @@ export const SITE_MENU: { title: string, href: string, target: string }[] = [
     { title: "友链", href: "./page/links/", target: "" },
     { title: "归档", href: "./page/archive/", target: "" },
     { title: "留言", href: "./page/guestbook", target: "" },
+    { title: "鱼塘", href: "./page/friends", target: "" },
     { title: "搜索", href: "./page/search", target: "" },
 ]
 
