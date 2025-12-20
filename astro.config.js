442135521e565aff992e831d22e2a5d1ac7fd285
LineXic <linexic@qq.com>
2025-12-20 14:40:04 +0800

    chore:删除链接显示插件

--- astro.config.js
+++ astro.config.js
@@ -3,33 +3,27 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
-import rehypeExternalLinks from 'rehype-external-links';
-
 import vercel from '@astrojs/vercel';
-
+// https://astro.build/config
 export default defineConfig({
-  build: {
-    format: "file",
-  },
-
+  // 请修改为你自己的线上地址，谢谢茄子
   site: 'https://www.linexic.top',
+
+  // 如果你的网站在子路径下（例如 `https://example.com/koi/`），则填写 `/koi/`
+  // 在根路径下（例如 `https://example.com/`）则填写 `/`
   base: process.env.NODE_ENV === "production" ? "/" : "",
-  integrations: [mdx(),sitemap(),svelte()],
+
+  integrations: [mdx(), sitemap(), svelte()],
 
   markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          content: { type: 'text', value: '🔗' }
-        },
-      ],
-    ],
+    remarkRehype: {
+      footnoteLabel: "脚注",
+      footnoteBackLabel: '文档内容的脚注',
+    }
   },
 
   vite: {
     plugins: [tailwindcss()]
   },
-
   adapter: vercel()
 });
